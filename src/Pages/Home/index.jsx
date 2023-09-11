d70477a9c86e5f24655ec24617ec9cf815809ec2
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-09-11 16:25:02 -0400

    consumiendo la FakeStore API para mostrar cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react';
 import Card from "../../Components/Card";
+import apiUrl from "../../api";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/products`);
+        const data = await response.json();
+        setItems(data);
+
+      } catch (error) {
+        console.error(`Ocurrio un error: ${error}`)
+      }
+    }
+    fetchData();
+  }, [])
+  
   return (
     <>
-      <div>
-        Home
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map((item) => {
+            return <Card key={item.id} data={item} />
+          })
+        }
       </div>
-        <Card />
     </>
   )
 }
