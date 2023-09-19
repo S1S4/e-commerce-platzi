ca15e187433a9c5a0514f3fbd7bb40e65ffd5d11
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-09-19 10:59:29 -0400

    contador de productos en el carrito

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 
 const Navbar = () => {
+    const context = useContext(ShoppingCartContext)
     const activeStyle = 'underline underline-offset-3';
 
     return (
@@ -93,7 +96,7 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink to="/">
-                        0000
+                        🛒 {context.count}
                     </NavLink>
                 </li>
             </ul>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Card from "../../Components/Card"
 import apiUrl from "../../api"
 
 function Home() {
-  const [items, setItems] = useState(null);
-
+  const [items, setItems] = useState(null)
   useEffect(() => {
     const fetchData = async () => {
       try {
