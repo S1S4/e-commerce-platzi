861defbcb53ef2cdf75739e3de03a2626c442dd5
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-09-22 19:35:16 -0400

    arreglado problema con ProductDetail que no se mostraba debido a que no pasaba las props correctas...

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -3,11 +3,12 @@ import { ShoppingCartContext } from "../../Context"
 
 const ProductDetail = () => {
     const context = useContext(ShoppingCartContext)
+    console.log('SHOW PRODCUCT: ', context.showProduct);
     return (
         <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} flex-col fixed right-0 border border-black bg-white rounded w-[360px] h-[calc(100vh-80px)]`}>
             <div className="flex justify-between items-center p-6">
                 <h2 className="font-medium text-xl">Detail</h2>
-                <div onClick={() => context.toggleProductDetail()}>
+                <div onClick={() => context.closeProductDetail()}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,16 +3,27 @@ import { createContext, useState } from "react"
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
+    // Shopping Cart - Increment quantity
     const [count, setCount] = useState(0);
+
+    // Product Detail - Open/Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
-    const toggleProductDetail = () => setIsProductDetailOpen(!isProductDetailOpen)
-    // const openProductDetail = () => setIsProductDetailOpen(true);
-    // const closeProductDetail = () => setIsProductDetailOpen(false);
+    const openProductDetail = () => setIsProductDetailOpen(true)
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+    // const toggleProductDetail = () => setIsProductDetailOpen(!isProductDetailOpen)
+
+    // Product Detail - Show product
+    const [showProduct, setShowProduct] = useState({});
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
             setCount,
-            toggleProductDetail
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            showProduct,
+            setShowProduct
         }}>
             {children}
         </ShoppingCartContext.Provider>
