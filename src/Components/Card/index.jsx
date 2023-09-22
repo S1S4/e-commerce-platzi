f28e8d8b3cab051c388c0db3dad45d9467ebf197
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-09-22 15:35:41 -0400

    maquetando el product detail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -4,13 +4,18 @@ import { ShoppingCartContext } from "../../Context"
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
     return (
-        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+        <div 
+            className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+            onClick={() => context.toggleProductDetail()}>
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data?.category?.name}</span>
                 <img className="w-full h-full object-cover rounded-lg" src={data.data?.images[0]} alt={data.data?.title} />
                 <div 
                     className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                    onClick={() => context.setCount(context.count + 1)}>
+                    onClick={(event) => {
+                        event.stopPropagation()
+                        context.setCount(context.count + 1)
+                    }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" />
                     </svg>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,10 +4,15 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
     const [count, setCount] = useState(0);
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const toggleProductDetail = () => setIsProductDetailOpen(!isProductDetailOpen)
+    // const openProductDetail = () => setIsProductDetailOpen(true);
+    // const closeProductDetail = () => setIsProductDetailOpen(false);
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            toggleProductDetail
         }}>
             {children}
         </ShoppingCartContext.Provider>
