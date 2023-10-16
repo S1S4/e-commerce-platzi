4ad1484fa367707f4067c97290762ea2c8a2cfb4
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-10-16 18:43:57 -0400

    pagina de myOrders: lista de ordenes

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { Link } from 'react-router-dom'
 import { ShoppingCartContext } from "../../Context"
 import Layout from "../../Components/Layout"
 import OrderCard from "../../Components/OrderCard"
@@ -10,7 +11,16 @@ function MyOrder() {
     <>
       <Layout>
         <div>
-          MyOrder
+          <div className='relative flex items-center justify-center w-80 mb-6'>
+            <Link to='/my-orders' className='absolute left-0'>
+              <span className='cursor-pointer'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                </svg>
+              </span>
+            </Link>
+            <h1>My Order</h1>
+          </div>
           <div className="flex flex-col w-80">
             {
               context.order?.slice(-1)[0].products.map(product => (
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,9 +1,29 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
+import Layout from '../../Components/Layout'
+import OrdersCard from '../../Components/OrdersCard'
+
 function MyOrders() {
+  const context = useContext(ShoppingCartContext)
+
   return (
     <>
-      <div>
-        MyOrders
-      </div>
+      <Layout>
+        <div className='relative flex items-center justify-center w-80'>
+          <h1>MyOrders</h1>
+        </div>
+        {
+          context.order?.map((order, index) => {
+            <Link key={index} to={`/my-orders/${order.id}`}>
+              <OrdersCard
+                totalPrice={order.totalPrice} 
+                totalProducts={order.totalProducts}
+              />
+            </Link>
+          })
+        }
+      </Layout>
     </>
   )
 }
