38a9d3217e2daa2180650d808d6bb692fcc7a845
S1S4 <Cesar_Zhiro26@hotmail.com>
2023-10-19 15:26:44 -0400

    pagina de myOrder: orden individual

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -7,6 +7,11 @@ import OrderCard from "../../Components/OrderCard"
 
 function MyOrder() {
   const context = useContext(ShoppingCartContext)
+  const currentPath = window.location.pathname
+  let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+
+  if (index === 'last') index = context.order?.length - 1
+
   return (
     <>
       <Layout>
@@ -14,7 +19,7 @@ function MyOrder() {
           <div className='relative flex items-center justify-center w-80 mb-6'>
             <Link to='/my-orders' className='absolute left-0'>
               <span className='cursor-pointer'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                 </svg>
               </span>
@@ -23,7 +28,7 @@ function MyOrder() {
           </div>
           <div className="flex flex-col w-80">
             {
-              context.order?.slice(-1)[0].products.map(product => (
+              context.order?.[index]?.products?.map(product => (
                 <OrderCard
                   key={product.id}
                   id={product.id}
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -14,14 +14,14 @@ function MyOrders() {
           <h1>MyOrders</h1>
         </div>
         {
-          context.order?.map((order, index) => {
-            <Link key={index} to={`/my-orders/${order.id}`}>
+          context.order?.map((order, index) => (
+            <Link key={index} to={`/my-orders/${index}`}>
               <OrdersCard
                 totalPrice={order.totalPrice} 
                 totalProducts={order.totalProducts}
               />
             </Link>
-          })
+          ))
         }
       </Layout>
     </>
